75796723aca7684fb8984bee2fa0462d3eabd146
Gyamerah-266 <102665481+Gyamerah-266@users.noreply.github.com>
2022-08-08 11:12:12 +0000

    check the sum of prime numbers < a given number

--- 104 code.py
+++ 104 code.py
@@ -1,12 +1,14 @@
-int(input("Enter a number:"))
-n = int 
+n = int(input("Enter a number:"))
+n = int(n) 
 sum=0
 for i in range(2,n+1):
     for j in range(2,i):
         if(i%j==0):
-            break
+             break
     else: 
         print(i,"is prime")
         sum=sum+i
-        print("sum of prime numbers is",sum)
+print("sum of prime numbers is",sum)
+        
+        
         
